d8c3ba26d794fcc71ba59616a738dad623699e3a
Deepesh Acharya <53911982+Deepeshac2890@users.noreply.github.com>
2022-12-26 19:08:14 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Google App Client Id */
-define('CLIENT_ID', '17823058979-hsruduq3n4c0p1urspcju9k446cjsn2e.apps.googleusercontent.com');
+define('CLIENT_ID', 'PUT_YOUR_OWN_CLIENT_ID');
 
 /* Google App Client Secret */
-define('CLIENT_SECRET', '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');
+define('CLIENT_SECRET', 'PUT_YOUR_OWN_SECRET');
 
 /* Google App Redirect Url */
 define('CLIENT_REDIRECT_URL', 'https://googlecalendertrial.deepeshacharya.repl.co/google-login.php');
